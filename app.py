44676fc9878239282fb91e25ac2893fcf853abdf
jalotra <jalotrashivam9@gmail.com>
2019-10-24 19:43:07 +0530

    app.py completed next to make a cool frontend

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 #  Lets quickly write a flask app that does something locally
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import requests
+import inference as infer
+
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-	return {'IMAGE_CLASS_ID' : 'id', 'IMAGE_CLASS_NAME' : 'name' }
+	if request.method == 'POST':
+		file = request.files['file']
+		image_bytes = file.read()
+		class_id, class_name = infer.get_prediction(image_bytes)
+		return jsonify({'class_id' : class_id, 'class_name' : class_name})
+
+	
 
 
 if __name__ == '__main__':
 	app.run(host = 'localhost', port = '5000', debug = True)
+
